af3a7f154c9832802131f8642ddb150ed309fa12
brandonpen <34911156+brandonpen@users.noreply.github.com>
2018-02-18 14:12:31 -0500

    - Added function to install blockchain drivers

--- fix_vega.ps1
+++ fix_vega.ps1
@@ -29,9 +29,31 @@ function cleanVegaDrivers {
 #* This function will install the AMD Adrenaline drivers 
 function installAdrenalineDrivers {
     'Installing Adrenaline drivers....' | Out-Host
+
+    # Grab only the first device
     $displays = Get-PnpDevice | Where-Object {($_.friendlyname -like 'Microsoft Basic Display Adapter' -and $_.Present) -or $_.friendlyname -like 'Radeon Vega Frontier Edition'} | select -Index 0
-    Start-Process -Wait -FilePath 'C:\Program Files (x86)\Windows Kits\10\Tools\x64\devcon.exe' -ArgumentList @(("update C:\AMD\Win10-64Bit-Radeon-Software-Adrenalin-Edition-18.1.1-Jan4\Packages\Drivers\Display\WT6A_INF\C0322612.inf " + $displays[0].HardwareID[0])) | Out-Null
+    
+    # Initiate a new Powershell process to install the drivers using Devcon
+    Start-Process -Wait -FilePath 'C:\Program Files (x86)\Windows Kits\10\Tools\x64\devcon.exe' -ArgumentList @(("update C:\AMD\Win10-64Bit-Radeon-Software-Adrenalin-Edition-18.1.1-Jan4\Packages\Drivers\Display\WT6A_INF\C0322612.inf " + $displays.HardwareID[0])) | Out-Null
+    
+    # Pause for 2 minutes to allow for any back-end system operations to clean up
     'Installation of Adrenaline drivers complete.' | Out-Host
     'Pausing for 2 minutes before next operations...' | Out-Host
     Start-Sleep -Seconds 120
+}
+
+#* This function will install the AMD Blockchain drivers
+function installBlockchainDrivers {
+
+    'Installing Blockchain drivers...' | Out-Host
+    # Grab only the first display
+    $displays = Get-PnpDevice | Where-Object {$_.friendlyname -like 'Radeon Vega Frontier Edition'} | Select -Index 0
+    
+    # Initiate a new Powershell process to install the drivers using Devcon
+    Start-Process -Wait -FilePath 'C:\Program Files (x86)\Windows Kits\10\Tools\x64\devcon.exe' -ArgumentList @(("update C:\amd\Win10-64Bit-Crimson-ReLive-Beta-Blockchain-Workloads-Aug23\Packages\Drivers\Display\WT6A_INF\C0317304.inf " + $displays.HardwareID[0])) | Out-Null
+    
+    # Pause for 2 minutes to allow for any back-end system operations to clean up
+    'Installation of Blockchain drivers complete.' | Out-Host
+    'Pausing for 2 minutes before next operation...' | Out-Host
+    start-sleep -Seconds 120
 }
