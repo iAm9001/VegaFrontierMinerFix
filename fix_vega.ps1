7847909f430dac1bdb7cbec14aa35644a89fac80
brandonpen <34911156+brandonpen@users.noreply.github.com>
2018-02-18 15:37:26 -0500

    - Prototype script complete

--- fix_vega.ps1
+++ fix_vega.ps1
@@ -1,7 +1,7 @@
-< # This script will uninstall your VEGA drivers with a fresh installation via DDU, 
+<# This script will uninstall your VEGA drivers with a fresh installation via DDU, 
 and then begin the process of performing the steps requried to make your Vega Frontier
 graphics cards perform adequately for mining with maximum hash rates.
-# > 
+#> 
 Param(
 [Parameter(Mandatory = $false, 
            
@@ -16,56 +16,58 @@ $ParameterName)
 # * This function will clean your AMD drivers from your system, without initiating a reboot.
 # * The rebooting operation will be handled via a workflow job instead.
 function cleanVegaDrivers {
-    'Cleaning AMD drivers..' | Out - Host
-    Set - Location C:\crypto\ddu 
-    ( & '.\Display Driver Uninstaller.exe' - silent - cleanamd) | Out - Null  # - restart
-    Start - Sleep - Seconds 15
+    'Cleaning AMD drivers..' | Out-Host
+    Set-Location C:\crypto\ddu 
+    (& '.\Display Driver Uninstaller.exe' -silent -cleanamd) | Out-Null  #-restart
+    Start-Sleep -Seconds 15
     
-    'sleeping for 10 seconds before rebooting after DDU...' | Out - Host
-    start - sleep - s 10
-    'rebooting...' | Out - Host
+    'sleeping for 10 seconds before rebooting after DDU...' | Out-Host
+    start-sleep -Seconds 10
+    'rebooting...' | Out-Host
 }
 
 # * This function will install the AMD Adrenaline drivers 
 function installAdrenalineDrivers {
-    'Installing Adrenaline drivers....' | Out - Host
+    'Installing Adrenaline drivers....' | Out-Host
 
-    # Grab only the first device
-    $displays = Get - PnpDevice | Where - Object {($_.friendlyname - like 'Microsoft Basic Display Adapter' - and $_.Present) - or $_.friendlyname - like 'Radeon Vega Frontier Edition'} | select - Index 0
+    # Grab only the last device
+    $displays = Get-PnpDevice | Where-Object {($_.friendlyname -like 'Microsoft Basic Display Adapter' -and $_.Present) -or $_.friendlyname -like 'Radeon Vega Frontier Edition'} | select -Index -1
     
     # Initiate a new Powershell process to install the drivers using Devcon
-    Start - Process - Wait - FilePath 'C:\Program Files (x86)\Windows Kits\10\Tools\x64\devcon.exe' - ArgumentList @(("update C:\AMD\Win10-64Bit-Radeon-Software-Adrenalin-Edition-18.1.1-Jan4\Packages\Drivers\Display\WT6A_INF\C0322612.inf " + $displays.HardwareID[0])) | Out - Null
+    Start-Process -Wait -FilePath 'C:\Program Files (x86)\Windows Kits\10\Tools\x64\devcon.exe' -ArgumentList @(("update C:\AMD\Win10-64Bit-Radeon-Software-Adrenalin-Edition-18.1.1-Jan4\Packages\Drivers\Display\WT6A_INF\C0322612.inf " + $displays.HardwareID[0])) | Out-Null
     
-    # Pause for 2 minutes to allow for any back - end system operations to clean up
-    'Installation of Adrenaline drivers complete.' | Out - Host
-    'Pausing for 2 minutes before next operations...' | Out - Host
-    Start - Sleep - Seconds 120
+    # Pause for 2 minutes to allow for any back-end system operations to clean up
+    'Installation of Adrenaline drivers complete.' | Out-Host
+    'Pausing for 2 minutes before next operations...' | Out-Host
+    Start-Sleep -Seconds 120
 }
 
 # * This function will install the AMD Blockchain drivers
 function installBlockchainDrivers {
 
-    'Installing Blockchain drivers...' | Out - Host
-    # Grab only the first display
-    $displays = Get - PnpDevice | Where - Object {$_.friendlyname - like 'Radeon Vega Frontier Edition'} | Select - Index 0
+    'Installing Blockchain drivers...' | Out-Host
+    
+    # Grab only the last display
+    $displays = Get-PnpDevice | Where-Object {$_.friendlyname -like 'Radeon Vega Frontier Edition'} | Select -Index -1
     
     # Initiate a new Powershell process to install the drivers using Devcon
-    Start - Process - Wait - FilePath 'C:\Program Files (x86)\Windows Kits\10\Tools\x64\devcon.exe' - ArgumentList @(("update C:\amd\Win10-64Bit-Crimson-ReLive-Beta-Blockchain-Workloads-Aug23\Packages\Drivers\Display\WT6A_INF\C0317304.inf " + $displays.HardwareID[0])) | Out - Null
+    Start-Process -Wait -FilePath 'C:\Program Files (x86)\Windows Kits\10\Tools\x64\devcon.exe' -ArgumentList @(("update C:\amd\Win10-64Bit-Crimson-ReLive-Beta-Blockchain-Workloads-Aug23\Packages\Drivers\Display\WT6A_INF\C0317304.inf " + $displays.HardwareID[0])) | Out-Null
     
-    # Pause for 2 minutes to allow for any back - end system operations to clean up
-    'Installation of Blockchain drivers complete.' | Out - Host
-    'Pausing for 2 minutes before next operation...' | Out - Host
-    start - sleep - Seconds 120
+    # Pause for 2 minutes to allow for any back-end system operations to clean up
+    'Installation of Blockchain drivers complete.' | Out-Host
+    'Pausing for 2 minutes before next operation...' | Out-Host
+    start-sleep -Seconds 120
 }
 
 # * Disables all Vega Frontier devices on the system, or optionall to skip disabling the firt one.
 # * Default is to disable; the function can be used to enable cards by specifying the EnableOperation switch.
 function ChangeVegaState {
     Param(
-       # Optional switch to instruct function to skip disabling / enabling the first Vega display adapter
+       # Optional switch to instruct function to skip disabling / enabling the last Vega display adapter
        [Parameter(Mandatory = $false)]
        [switch]
-       $SkipFirstVega, 
+       $SkipLastVega,
+      # $SkiplastVega, 
 
        # Specify to perform enable operations instead
        [Parameter(Mandatory = $false)]
@@ -83,20 +85,20 @@ function ChangeVegaState {
 
      
     # Get all Vega frontier display adapters
-	$displays = Get - PnpDevice | Where - Object {$_.friendlyname - like 'Radeon Vega Frontier Edition'}
+	$displays = Get-PnpDevice | Where-Object {$_.friendlyname -like 'Radeon Vega Frontier Edition'}
     
-    # Remove the first Vega display adapter from the list if the SkipFirstVega switch was specified
-    if ($SkipFirstVega) {
+    # Remove the last Vega display adapter from the list if the SkiplastVega switch was specified
+    if ($SkipLastVega) {
 
-    'Getting primary Vega...' | Out - Host
+    'Getting last Vega...' | Out-Host
 
     # Establish the primary Vega display adapter from index 0
-    $mainVega = $displays[0]
+    $lastVega = $displays[-1]
     
-    'Primary Vega identified as ' + $mainVega.HardwareID | Out - Host
+    'Last Vega identified as ' + $lastVega.HardwareID | Out-Host
 
-    # Set the collection of all of the displays to equal itself minus the first display
-    $displays = $displays | Where - Object {$_ - ne $mainVega}
+    # Set the collection of all of the displays to equal itself minus the last display
+    $displays = $displays | Where-Object {$_ -ne $lastVega}
     }
 
     # Initialize counter for console output
@@ -143,7 +145,7 @@ function DisableCrossfireUlps {
     'Disabling Crossfire and ULPS on all Vega...' | Out-Host
 
     # Set the path to your Vega Frontier registry keys here
-    $regKeyPath = 'SYSTEM\CurrentControlSet\Control\Class\ {4d36e968 - e325-11ce - bfc1-08002be10318}'
+    $regKeyPath = 'SYSTEM\CurrentControlSet\Control\Class\ {4d36e968-e325-11ce-bfc1-08002be10318}'
 
     # Navigate to the Windows Registry
     Set-Location HKLM:\$regKeyPath
@@ -153,8 +155,8 @@ function DisableCrossfireUlps {
     
     # Navigate to each Vega Frontier display adapter registry path, that ends with a numbered
     # display adapter, and disable Crossfire and Ulps.
-    foreach ($k in $keyPaths | Where-Object {$_.name -match '\\[\d] {4}$' } ){
-        
+    foreach ($k in $keyPaths | Where-Object {$_.name -match '\\[\d]{4}$' } ){
+
         # Set the location to the path of the adapter key
         Set-Location $k.PSPath | Out-Null    
         
@@ -167,3 +169,49 @@ function DisableCrossfireUlps {
         'Next...' | Out-Host
     }
 }
+
+#* This is a workflow that will execute in the background after windows reboots to resume the Vega display adapter
+#* repair process.
+workflow VegaFixWorkflow {
+    
+    # Clean the Vega drivers from your system
+    cleanVegaDrivers
+    Restart-Computer -Force -Wait
+
+    # Install the Adrenaline drivers
+    installAdrenalineDrivers
+
+    # Disable Crossfire and Ulps on all cards
+    DisableCrossfireUlps
+
+    # Disable all Vega display adapters except for the last adapter
+    ChangeVegaState -SkipLastVega
+
+    # Disable Crossfire and Ulps on all cards again, just in case...
+    DisableCrossfireUlps
+
+    # Enable all Vega adapters except for the last adapter (should already been enabled)
+    ChangeVegaState -EnableOperation -SkipLastVega
+
+    # Finished!
+    'Operation complete.' | Out-Host
+
+    return
+}
+# Create the scheduled job properties
+$options = New-ScheduledJobOption -RunElevated -ContinueIfGoingOnBattery -StartIfOnBattery
+
+#$secpasswd = ConvertTo-SecureString "yourPasswordHere" -AsPlainText -Force
+
+# Get the credentials of the current user to use for automatic workflow exceution
+$credentials = Get-Credential -UserName ($env:COMPUTERNAME.ToString() + '\' + $env:USERNAME)
+
+#$credentials = New-Object System.Management.Automation.PSCredential ($env:COMPUTERNAME.ToString() +"\brand", $secpasswd)
+$AtStartup = New-JobTrigger -AtStartup
+
+# Register the scheduled job
+Register-ScheduledJob -Name Resume_Workflow_Job -Trigger $AtStartup -Credential $credentials -ScriptBlock ({[System.Management.Automation.Remoting.PSSessionConfigurationData]::IsServerManager = $true; Import-Module PSWorkflow; Resume-Job -Name new_resume_workflow_job -Wait}) -ScheduledJobOption $options
+
+# Execute the workflow as a new job
+Resume_Workflow -AsJob -JobName new_resume_workflow_job
+
