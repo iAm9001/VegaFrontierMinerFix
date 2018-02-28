d6d557373fca0df4fe54fcc8c782cab087c7c5de
brandonpen <34911156+brandonpen@users.noreply.github.com>
2018-02-27 20:10:03 -0500

    - Added 15 second sleep to the end to give the job a chance to run

--- ResumeWF-Job.ps1
+++ ResumeWF-Job.ps1
@@ -1,4 +1,5 @@
 #* This is the script that will initiate the workflow upon the computer restarting
 
-$jobs = Get-Job -state Suspended | Where-Object {$_.name -like '*vega*'}
-$jobs | resume-job
+$job = Get-Job -Name 'ResumeVegaFixWorkflow' | Where-Object {$_.State -eq 'Suspended'} | select -Index 0
+Resume-Job $job.id
+Start-Sleep -Seconds 15
