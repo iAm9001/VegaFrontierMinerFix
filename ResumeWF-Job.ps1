7f8a97e854f28fb77481fac200f0ced7b39f7bbc
brandonpen <34911156+brandonpen@users.noreply.github.com>
2018-02-27 20:11:44 -0500

    - Works, testing one more time

--- ResumeWF-Job.ps1
+++ ResumeWF-Job.ps1
@@ -3,3 +3,9 @@
 $job = Get-Job -Name 'ResumeVegaFixWorkflow' | Where-Object {$_.State -eq 'Suspended'} | select -Index 0
 Resume-Job $job.id
 Start-Sleep -Seconds 15
+
+#! Execute task removal code one more time for the sake of redundancy, will figure out where the
+#! best place to remove the task is later
+
+#* Remove any stray scheduled tasks from previous runs
+Get-ScheduledTask -TaskName ResumeWFJobTask -ErrorAction SilentlyContinue | Unregister-ScheduledTask -Confirm:$false
