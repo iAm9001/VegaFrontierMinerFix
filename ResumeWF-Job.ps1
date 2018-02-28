8cc106df420fe205d4dc9268a517a8e5ce99de2c
iAm9001 <iAm9001@outlook.com>
2018-02-27 19:12:06 -0500

    - Tweaked to fit more in line with the way the parent script behaves

--- ResumeWF-Job.ps1
+++ ResumeWF-Job.ps1
@@ -1,6 +1,5 @@
 #* This is the script that will initiate the workflow upon the computer restarting
 
-Import-Module –Name PSWorkflow
-$jobs = Get-Job -state Suspended
-$resumedJobs = $jobs | resume-job -wait
-$resumedJobs | wait-job
+#Import-Module –Name PSWorkflow
+$jobs = Get-Job -state Suspended | Where-Object {$_.name -like '*vega*'}
+$resumedJobs = $jobs | resume-job
