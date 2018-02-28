623ac4f5ec194779eccca0433e08eff57216fd39
brandonpen <34911156+brandonpen@users.noreply.github.com>
2018-02-27 20:13:17 -0500

    - Appended 'wait' argument instead of issuing a sleep command

--- ResumeWF-Job.ps1
+++ ResumeWF-Job.ps1
@@ -1,8 +1,7 @@
 #* This is the script that will initiate the workflow upon the computer restarting
 
 $job = Get-Job -Name 'ResumeVegaFixWorkflow' | Where-Object {$_.State -eq 'Suspended'} | select -Index 0
-Resume-Job $job.id
-Start-Sleep -Seconds 15
+Resume-Job $job.id -Wait
 
 #! Execute task removal code one more time for the sake of redundancy, will figure out where the
 #! best place to remove the task is later
