4bfda9b895b7a4b6b2832fab6a8ebabd3dd2f6fb
Brandon Pen <brandon.pen@outlook.com>
2018-03-07 12:49:49 -0500

    - Sleep for 10 seconds before getting job to resume

--- ResumeWF-Job.ps1
+++ ResumeWF-Job.ps1
@@ -1,5 +1,7 @@
 #* This is the script that will initiate the workflow upon the computer restarting
 
+Start-Sleep -Seconds 10
+
 # Acquire teh job to wait on ....
 $job = Get-Job -Name ResumeVegaFixWorkflow | Where-Object {$_.State -eq 'Suspended'} | select -Index 0
 Resume-Job $job.id
